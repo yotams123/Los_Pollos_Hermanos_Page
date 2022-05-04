aebd41a4031b66bfb4ef2dee50bcb53dc7f54c62
yotams123 <48293852+yotams123@users.noreply.github.com>
2022-05-04 17:52:28 +0300

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-﻿INSERT INTO [Orders] VALUES (' (ABQ Sandwich, $8.00, with BBQ Sauce), (Chicken Tenders, $7.50, with BBQ Sauce), (Fring Fries, $7.00, with BBQ Sauce), (Slaw Goodman, $6.00, with BBQ Sauce), ', '28.50', '
-Yotam
-', '
-Outside of the USA
-', '
-0555555555
-');
-SELECT * FROM [Orders];
